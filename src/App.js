2522e1afeeaac23fa64b130ef5d525b124c74ab1
unknown <canberksahin20@gmail.com>
2023-01-19 15:56:16 +0300

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Selam, bu kısmı değiştirilebilir!
+          Selam, bu kısmı yeni branch ile değiştirip sonrasına master branchine PR ile merge ettim.
         </p>
         <a
           className="App-link"
